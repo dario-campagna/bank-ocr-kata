f98d9d32d6f4c52aad49db7f629b28c180cec44c
Dario Campagna <campagna@esteco.com>
2017-07-27 10:48:39 +0200

    Introduce CellConverter.

---
+++ production/source/it/esteco/bankocr/CellConverter.java
@@ -0,0 +1,72 @@
+package it.esteco.bankocr;
+
+public class CellConverter {
+
+    private static final String ZERO_CELL =
+            " _ " +
+                    "| |" +
+                    "|_|";
+    private static final String ONE_CELL =
+            "   " +
+                    "  |" +
+                    "  |";
+    private static final String TWO_CELL =
+            " _ " +
+                    " _|" +
+                    "|_ ";
+    private static final String THREE_CELL =
+            " _ " +
+                    " _|" +
+                    " _|";
+    private static final String FOUR_CELL =
+            "   " +
+                    "|_|" +
+                    "  |";
+    private static final String FIVE_CELL =
+            " _ " +
+                    "|_ " +
+                    " _|";
+    private static final String SIX_CELL =
+            " _ " +
+                    "|_ " +
+                    "|_|";
+    private static final String SEVEN_CELL =
+            " _ " +
+                    "  |" +
+                    "  |";
+    private static final String EIGHT_CELL =
+            " _ " +
+                    "|_|" +
+                    "|_|";
+    private static final String NINE_CELL =
+            " _ " +
+                    "|_|" +
+                    " _|";
+
+    public static String toDigit(String cell) {
+        switch (cell) {
+            case ZERO_CELL:
+                return "0";
+            case ONE_CELL:
+                return "1";
+            case TWO_CELL:
+                return "2";
+            case THREE_CELL:
+                return "3";
+            case FOUR_CELL:
+                return "4";
+            case FIVE_CELL:
+                return "5";
+            case SIX_CELL:
+                return "6";
+            case SEVEN_CELL:
+                return "7";
+            case EIGHT_CELL:
+                return "8";
+            case NINE_CELL:
+                return "9";
+        }
+        return "";
+    }
+
+}
--- production/source/it/esteco/bankocr/Entry.java
+++ production/source/it/esteco/bankocr/Entry.java
@@ -2,47 +2,6 @@ package it.esteco.bankocr;
 
 public class Entry {
 
-    private static final String ZERO_CELL =
-            " _ " +
-            "| |" +
-            "|_|";
-    private static final String ONE_CELL =
-            "   " +
-            "  |" +
-            "  |";
-    private static final String TWO_CELL =
-            " _ " +
-            " _|" +
-            "|_ ";
-    private static final String THREE_CELL =
-            " _ " +
-            " _|" +
-            " _|";
-    private static final String FOUR_CELL =
-            "   " +
-            "|_|" +
-            "  |";
-    private static final String FIVE_CELL =
-            " _ " +
-            "|_ " +
-            " _|";
-    private static final String SIX_CELL =
-            " _ " +
-            "|_ " +
-            "|_|";
-    private static final String SEVEN_CELL =
-            " _ " +
-            "  |" +
-            "  |";
-    private static final String EIGHT_CELL =
-            " _ " +
-            "|_|" +
-            "|_|";
-    private static final String NINE_CELL =
-            " _ " +
-            "|_|" +
-            " _|";
-
     private final String firstLine;
     private final String secondLine;
     private final String thirdLine;
@@ -62,34 +21,8 @@ public class Entry {
             return "";
         } else {
             String cell = firstLine.substring(0, 3) + secondLine.substring(0, 3) + thirdLine.substring(0, 3);
-            return cellToDigit(cell) + convert(firstLine.substring(3), secondLine.substring(3), thirdLine.substring(3));
+            return CellConverter.toDigit(cell) + convert(firstLine.substring(3), secondLine.substring(3), thirdLine.substring(3));
         }
-    }
-
-    private String cellToDigit(String cell) {
-        switch (cell) {
-            case ZERO_CELL:
-                return "0";
-            case ONE_CELL:
-                return "1";
-            case TWO_CELL:
-                return "2";
-            case THREE_CELL:
-                return "3";
-            case FOUR_CELL:
-                return "4";
-            case FIVE_CELL:
-                return "5";
-            case SIX_CELL:
-                return "6";
-            case SEVEN_CELL:
-                return "7";
-            case EIGHT_CELL:
-                return "8";
-            case NINE_CELL:
-                return "9";
-        }
-        return "";
     }
 
     @Override
