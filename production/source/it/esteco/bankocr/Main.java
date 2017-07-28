416b5ed410c20fab391fb43d2043a2ea528e89a1
Dario Campagna <campagna@esteco.com>
2017-07-28 07:40:44 +0200

    We now write valid account numbers to file.

--- production/source/it/esteco/bankocr/Main.java
+++ production/source/it/esteco/bankocr/Main.java
@@ -3,12 +3,14 @@ package it.esteco.bankocr;
 import it.esteco.bankocr.adapters.FileEntryReader;
 import it.esteco.bankocr.domain.BankOCR;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
 public class Main {
 
-    public static final int FILE_PATH_AS_TEXT = 0;
+    private static final int INPUT_FILE_PATH_AS_TEXT = 0;
+    private static final int OUTPUT_FILE_PATH_AS_TEXT = 1;
     private BankOCR bankOCR;
 
     public Main() {
@@ -17,13 +19,22 @@ public class Main {
 
     public static void main(String... args) {
         Main main = new Main();
-        main.parse(args[FILE_PATH_AS_TEXT]);
+        main.convert(args[INPUT_FILE_PATH_AS_TEXT], args[OUTPUT_FILE_PATH_AS_TEXT]);
     }
 
-    private void parse(String filePathAsText) {
+    private void convert(String inputFilePathAsText, String outputFilePathAsText) {
         try {
-            List accountNumbers = bankOCR.parse(new FileEntryReader(filePathAsText));
-            accountNumbers.stream().forEach(accountNumberAsText -> System.out.println(accountNumberAsText));
+            List accountNumbers = bankOCR.parse(new FileEntryReader(inputFilePathAsText));
+            FileWriter outputWriter = new FileWriter(outputFilePathAsText);
+            accountNumbers.stream().forEach(accountNumberAsText -> {
+                try {
+                    outputWriter.write(accountNumberAsText + "\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            outputWriter.flush();
+            outputWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
--- test/source/test/banckocr/ApplicationRunner.java
+++ test/source/test/banckocr/ApplicationRunner.java
@@ -4,6 +4,7 @@ import it.esteco.bankocr.Main;
 
 import java.io.*;
 import java.net.URL;
+import java.nio.file.Files;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
@@ -11,35 +12,23 @@ import static org.junit.Assert.assertThat;
 
 public class ApplicationRunner {
 
-    private final ByteArrayOutputStream byteArrayOutputStream;
-    private BufferedReader consoleReader;
-    private PrintStream console;
+    private static final String OUTPUT_FILE = "out";
+    private BufferedReader outputReader;
 
-    public ApplicationRunner() {
-        byteArrayOutputStream = new ByteArrayOutputStream();
-        changeSystemOutTo(byteArrayOutputStream);
-    }
 
-    public void parseFile(URL allZerosSingleEntry) {
-        Main.main(allZerosSingleEntry.getFile());
-        consoleReader = createBufferReaderFor(byteArrayOutputStream);
+    public void parseFile(URL fileURL) throws FileNotFoundException {
+        Main.main(fileURL.getFile(), OUTPUT_FILE);
+        FileReader fileReader = new FileReader(OUTPUT_FILE);
+        outputReader = new BufferedReader(fileReader);
     }
 
     public void showsAccountNumber(String accountNumberAsText) throws IOException {
-        assertThat(consoleReader.readLine(), is(equalTo(accountNumberAsText)));
+        assertThat(outputReader.readLine(), is(equalTo(accountNumberAsText)));
     }
 
-    public void stop() {
-        System.setOut(console);
+    public void stop() throws IOException {
+        outputReader.close();
+        Files.deleteIfExists(new File(OUTPUT_FILE).toPath());
     }
 
-    private void changeSystemOutTo(ByteArrayOutputStream byteArrayOutputStream) {
-        console = System.out;
-        System.setOut(new PrintStream(byteArrayOutputStream));
-    }
-
-    private BufferedReader createBufferReaderFor(ByteArrayOutputStream byteArrayOutputStream) {
-        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
-        return new BufferedReader(new InputStreamReader(byteArrayInputStream));
-    }
 }
